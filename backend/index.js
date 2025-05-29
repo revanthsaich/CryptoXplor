a2009a18be02b7ce25399144db550ea3bcc4e09e
revanthsaich <revanthsaichaparala05@gmail.com>
2025-05-29 20:51:17 +0530

    deployment v4.0

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const marketRoutes = require('./routes/market');
 
 //added this to allow CORS requests from the frontend domain
 app.use(cors({
-  origin: ['http://localhost:5173','https://crypto-xplor.vercel.app'],
+  origin: ['http://localhost:5173','https://cryptoxplor.netlify.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Clerk-Session-Id'],
   credentials: true,
